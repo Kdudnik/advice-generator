3a96a6b89600598b75c05d1c4fc4cb600beab24e
Kiril Dudnik <kdudnik17@gmail.com>
2023-09-03 15:37:16 +0200

    feature: add localStorage

--- js/script.js
+++ js/script.js
@@ -2,14 +2,23 @@ const adviceId = document.querySelector('.advice__number')
 const adviceField = document.querySelector('.card__advice')
 const generateAdviceBtn = document.querySelector('.card__dice__btn')
 
+if(localStorage) uploadFromLocalStorage()
+else getAdvice()
+
 async function getAdvice() {
     fetch("	https://api.adviceslip.com/advice").then(obj => {
         return obj.json()
     }).then(result => {
         adviceId.innerHTML = `#${result.slip.id}`
         adviceField.innerHTML = `"${result.slip.advice}"`
+        localStorage.setItem('activeAdviceId', result.slip.id)
+        localStorage.setItem('activeAdvice', result.slip.advice)
     })
 }
-getAdvice() // init
+
+function uploadFromLocalStorage() {
+    adviceId.innerHTML = `#${localStorage.getItem("activeAdviceId")}`
+    adviceField.innerHTML = `"${localStorage.getItem("activeAdvice")}"`
+}
 
 generateAdviceBtn.addEventListener('click', () => getAdvice())
